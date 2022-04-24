5cb793faa489d44973c3dd8548094a4436de7f79
sauravmaximus <sauravmaximus@gmail.com>
2022-04-23 17:15:30 -0700

    modified parallel parsing functionality

--- hydragrep.py
+++ hydragrep.py
@@ -1,21 +1,24 @@
 import concurrent.futures
+import os
 from pathlib import Path
 import sys
 import logging
 from time import time
 from argparse import ArgumentParser
+from pprint import pprint
+from time import time
 
 from pattern_searcher import PatternSearcher
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-IGNORED_FILE_TYPE_EXTENSIONS = frozenset([".md", ".gz", ".bz2", ".zip", ".pdf", ".jpg", ".png", ".js", ".txt", ".css"])
+IGNORED_FILE_TYPE_EXTENSIONS = frozenset([".md", ".gz", ".bz2", ".zip", ".pdf", ".jpg", ".png", ".txt", ".css", ".pyc"])
 IGNORED_FILES = lambda x: not x.suffix in IGNORED_FILE_TYPE_EXTENSIONS
 OPTIMIZED_CHUNKSIZE = 500
 
 THREAD_COUNT = 1
-MAX_DEPTH = 4
+MAX_DEPTH = 10
 
 class HydraParser(object):
     def __list_files_in_dir(self, dirname):
@@ -25,7 +28,34 @@ class HydraParser(object):
         except Exception as e:
             logger.error("Exception occurred: {}", e)
             sys.exit(1)
+
+    def get_home_dir(self):
+        return str(Path.home())
     
+    def accessible(self, dir):
+        return os.path.isdir(dir) and os.access(dir, os.R_OK) and not os.path.basename(dir).startswith(".") \
+            and not os.path.basename(dir).startswith("node_modules") \
+                and not os.path.basename(dir).startswith("Library")  \
+                    and not os.path.basename(dir).startswith("Envs")
+    
+    def build_directory_index(self, dir):
+        t1 = time()
+        results = self._fast_scandir(dir)
+        t2 = time()
+        print("Time required to build directory index: {} seconds".format((t2-t1)))
+        return results
+    
+    def _fast_scandir(self, dirname):
+        if not dirname:
+            return []
+        if not self.accessible(dirname):
+            return []
+        subfolders= [f.path for f in os.scandir(dirname) if os.path.isdir(f.path)]
+        
+        for dirname in list(subfolders):
+            subfolders.extend(self._fast_scandir(dirname))
+        return subfolders
+
     def recursively_get_files(self, dirname, results, level_count):
         if level_count == MAX_DEPTH:
             return
@@ -40,18 +70,16 @@ class HydraParser(object):
                 results.append(file)
 
     def search(self, dirname, pattern):
-        t1 = time()
         results = list()
         self.recursively_get_files(dirname, results, 0)
-        print("results: ", len(results))
         pattern_searcher = PatternSearcher(pattern)
         # run parallel searches using a concurrent process pool executor
         with concurrent.futures.ProcessPoolExecutor() as executor:
-            executor.map(pattern_searcher.search_in_file, results, chunksize=OPTIMIZED_CHUNKSIZE)
-
-        t2 = time()
-        print("Searching for {} in the directory {} took {} seconds".format(pattern, dirname, (t2-t1)))
-
+            for r in executor.map(pattern_searcher.search_in_file, results, chunksize=OPTIMIZED_CHUNKSIZE):
+                if not r:
+                    continue
+                else:
+                    yield r
 
 if __name__ == "__main__":
     hydra = HydraParser()
